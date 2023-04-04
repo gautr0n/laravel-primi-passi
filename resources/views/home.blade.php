2ff0f3c02c9be7f007b56b1ad04114608a6d9537
Giorgio.Bocchio <giorgiobocchio2@gmail.com>
2023-04-04 16:04:59 +0200

    add hello world and a paragraph

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="imgs/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="imgs/favicon.ico" type="image/x-icon">
+  <title>Php Oop 2</title>
+  <link rel="stylesheet" href="./style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+</head>
+
+<body class="container mt-5">
+    <h1>Hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In excepturi tempore, deleniti, iure, beatae facere laboriosam dicta culpa cumque repudiandae dolores consectetur delectus! Odio aliquam maxime at atque blanditiis similique!</p>
+</body>
+
+</html>
